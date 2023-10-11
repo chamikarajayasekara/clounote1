ee06f01b436c4e27a9e864fc1d34d509a097e52e
chamikara <51282162+chamikarajayasekara@users.noreply.github.com>
2023-10-11 17:54:43 +0530

    mobile drawer button added

--- src/components/templates/HomePagetemplates/HomePageHeroSection/HomePageHeroSection.tsx
+++ src/components/templates/HomePagetemplates/HomePageHeroSection/HomePageHeroSection.tsx
@@ -4,7 +4,7 @@ import HeroSlider from "../../../organisms/Sliders/HeroSLider/HeroSlider";
 import hero1 from "../../../../assets/hss1.png";
 import hero2 from "../../../../assets/h3.png";
 import hero3 from "../../../../assets/h4.png";
-import menu from "../../../../assets/menu.png";
+import menu from "../../../../assets/dmb.png";
 import MobileDrawer from '../../../organisms/MobileDarwer/MobileDrawer';
 import IconButton from "../../../atoms/Buttons/IconButton/IconButton";
 
@@ -28,9 +28,12 @@ const HomePageHeroSection = () => {
         <div className="hero-section">
             <NavigationBar/>
             <HeroSlider images={images}/>
-            <span onClick={handleOpenDrawer} className="drawer-button">
-                 <IconButton name={'menu'} icon={menu} width={20} height={20}/>
-            </span>
+            <div className="drawer-round-button">
+                  <div onClick={handleOpenDrawer} className="drawer-button">
+                        <IconButton name={'menu'} icon={menu} width={20} height={20}/>
+                    </div>
+            </div>
+
             {
                 isDrawerOpen ? <MobileDrawer isOpen={isDrawerOpen} onClose={handleCloseDrawer} /> : null
             }
