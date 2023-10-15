8c295da00dca21a9a895930bde139495a2e533f9
chamikara <51282162+chamikarajayasekara@users.noreply.github.com>
2023-10-15 13:33:59 +0530

    Desktop UI issues fixed

--- src/components/organisms/Contents/FooterContents/FooterContentFour.tsx
+++ src/components/organisms/Contents/FooterContents/FooterContentFour.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {Col, Row} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 const FooterContentFour = () => {
+    const handleCRedirectToClounote = () => {
+        window.location.replace('https:/clounote.com');
+    };
     return (
         <Row className="footer-bottom-row">
             <Col md={4}>
-                <span>© kittour.com 2015-2021. All rights reserved.</span>
+                <span className="all-right-res">© kittour.com 2015-2021. All rights reserved.</span>
             </Col>
             <Col md={4}>
-                <span>Designed and developed by Clounote.com</span>
+                <span>Designed and developed by <a onClick={handleCRedirectToClounote}>www.clounote.com</a></span>
             </Col>
             <Col md={4}>
                <span>Contact Us   &nbsp; &nbsp;| &nbsp;&nbsp;  Terms of Use&nbsp;&nbsp;   |&nbsp;&nbsp;   Privacy Policy</span>
