d9e191a9a7e1edecdf65f9834c2006e64e28da06
chamikara <51282162+chamikarajayasekara@users.noreply.github.com>
2023-12-03 16:57:59 +0530

    Mobile smmoth added things to do

--- src/pages/ThingsToDo/ThingsToDo.tsx
+++ src/pages/ThingsToDo/ThingsToDo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Breadcrumb from "../../components/molecules/Breadcrumb/Breadcrumb";
 import PageHeader from "../../components/atoms/Headers/PageHeader";
 import {Col, Container, Row} from "react-bootstrap";
@@ -7,11 +7,22 @@ import MainLayoutThingToDo from "../../components/templates/ThingsToDo/MainLayou
 import Banner1 from "../../components/organisms/Banners/Banner1/Banner1";
 import banner  from "../../assets/ttdbanner.png";
 import DiscoverExperience from "../../components/templates/ThingsToDo/DiscoverExperience/DiscoverExperience";
+import MobileMenu from "../../components/molecules/MenuSection/MobileMenu/MobileMenu";
+import MobileDrawer from "../../components/organisms/MobileDarwer/MobileDrawer";
 
 const ThingsToDo = () => {
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    const handleOpenDrawer = () => {
+        setIsDrawerOpen(true);
+    };
+    const handleCloseDrawer = () => {
+        setIsDrawerOpen(false);
+    };
+
     return (
         <>
             <div className="things-to-do-page">
+                <MobileMenu handleOpenDrawer={handleOpenDrawer}/>
                 <Breadcrumb paths={ThingsToDoDataset.paths} />
                 <PageHeader title={'Discover things to do'} mobileShow={true}/>
                 <Container>
@@ -22,22 +33,25 @@ const ThingsToDo = () => {
                     </Row>
                 </Container>
             </div>
-            {/*<Banner1*/}
-            {/*    borderRadius={'0'}*/}
-            {/*    paddingTop={'75'}*/}
-            {/*    paddingLeft={'90'}*/}
-            {/*    imageUrl={banner}*/}
-            {/*    height={'500px'}*/}
-            {/*    descriptionSideBorder={false}*/}
-            {/*    header={ThingsToDoDataset.bannerText.question}*/}
-            {/*    description={ThingsToDoDataset.bannerText.description}*/}
-            {/*    buttonDescription={ThingsToDoDataset.bannerText.buttonText}*/}
-            {/*    leftSideLength={'540px'}*/}
-            {/*    h1FontSize={'67px'}*/}
-            {/*    h1LineHeight={'83px'}*/}
-            {/*    pFont={'18px'}*/}
-            {/*/>*/}
+            <Banner1
+                borderRadius={'0'}
+                paddingTop={'75'}
+                paddingLeft={'90'}
+                imageUrl={banner}
+                height={'500px'}
+                descriptionSideBorder={false}
+                header={ThingsToDoDataset.bannerText.question}
+                description={ThingsToDoDataset.bannerText.description}
+                buttonDescription={ThingsToDoDataset.bannerText.buttonText}
+                leftSideLength={'540px'}
+                h1FontSize={'67px'}
+                h1LineHeight={'83px'}
+                pFont={'18px'}
+            />
             <DiscoverExperience item={ThingsToDoDataset.banner2}/>
+            {
+                isDrawerOpen ? <MobileDrawer isOpen={isDrawerOpen} onClose={handleCloseDrawer} /> : null
+            }
         </>
 
     );
