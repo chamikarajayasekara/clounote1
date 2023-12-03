07dad55df401d5275c343304f6b3fbbd32799267
chamikara <51282162+chamikarajayasekara@users.noreply.github.com>
2023-12-03 17:48:48 +0530

    Update MenuSection.tsx

--- src/components/molecules/MenuSection/MenuSection.tsx
+++ src/components/molecules/MenuSection/MenuSection.tsx
@@ -6,18 +6,18 @@ interface Props {
 const MenuSection = (props:Props) => {
     const location = useLocation();
     const menuItems = [
-        {name:'Things to do', navigation:'things-to-do'},
-        {name:'Hotels',navigation:'hotels'},
-        {name:'Tours',navigation:'tours'},
-        {name:'About Us', navigation:'about-us'},
-        {name:'Contact Us', navigation:'contact-us'}
+        {name:'Things to do', navigation:'/things-to-do'},
+        {name:'Hotels',navigation:'/hotels'},
+        {name:'Tours',navigation:'/tours'},
+        {name:'About Us', navigation:'/about-us'},
+        {name:'Contact Us', navigation:'/contact-us'}
     ]
 
     return (
         <div className={props.mobile? "menu-section-mobile" : "menu-section"}>
             {
                 menuItems.map((menu:any, key:number) =>
-                    <div className={location.pathname.split('/')[1] === menu.navigation ? `list-styles-active`: 'list-styles'} key={key} >
+                    <div className={location.pathname.split('/')[1] === menu.navigation.split('/')[1] ? `list-styles-active`: 'list-styles'} key={key} >
                         <div className="menu-item">
                             <Link  to={menu.navigation} className="link-tag">
                                 <div className={`menu-item-name active-menu`}>
